fd9168fa6117ece8e28c8bdf05e798ff6298a5cf
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-12-13 15:03:44 +0000

    updates

---
+++ fourth_assessment/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="navbar">
+    <span>{{  logo }}</span>
+    <ul>
+        <li v-for="item in ul">{{ item }}</li>
+</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data() {
+    return {
+      logo: 'rendezvous',
+      ul: ['Discover', 'About us', 'FAQs', 'Contact us'],
+      button: ['Log in', 'Sign up']
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
