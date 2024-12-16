043b25a16f177f344fd911a1e230d5363b6d2108
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-12-16 13:57:43 +0000

    updates

--- fourth_assessment/src/SingleProduct.vue
+++ fourth_assessment/src/SingleProduct.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
-      <h1>{{ product.title }}</h1>
+    <div class="wrapper">
+      <h3>{{ product.title }}</h3>
       <img :src="product.image" alt="">
       <p>{{ product.description }}</p>
       <p>Price: {{ product.price }}</p>
+      <button>Add to Cart</button>
     </div>
   </template>
   
@@ -22,3 +23,36 @@
     }
   }
   </script>
+
+  <style scoped>
+  .wrapper{
+    margin: auto;
+    width: 30%;
+    border-radius: 10px;
+    box-shadow: 1px 1px 8px rgba(204, 169, 228, 0.5);
+
+  }
+  .wrapper img{
+    border-top-right-radius: 10px ;
+    border-top-left-radius: 10px ;
+    max-width:50%;
+    height: auto;
+    margin: auto;
+    display: block;
+    margin-top: 17px;
+}
+.wrapper h3{
+    font-weight: bolder;
+    font-family: Arial;
+    font-size: 16px;
+    margin: 15px;
+    text-align: center;
+}
+.wrapper p{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    font-size: 14px;
+    padding: 20px;
+    text-align: justify;
+}
+   
+</style>
